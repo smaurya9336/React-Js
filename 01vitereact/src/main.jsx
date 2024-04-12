330e45fa679111a0e6ab82e7af1d40d7f37a78d5
Sarita Maurya <sm7767119@gmail.com>
2024-04-12 13:26:13 +0530

    library

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -14,19 +14,21 @@ const anotherElement = (
     <a href="https://google.com" target='_blank'>visit google</a>
 )
 
-
+const anotherUser= "chai aur code"
 const reactElement = React.createElement(
     'a',
     {href :'https://google.com', target :'_blank'},
-    'click me to visite google'
+    'click me to visite google',
+    anotherUser,
+    anotherElement
 )
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     // anotherElement
-    // reactElement
-    <App />
+    reactElement
+    // <App />
   
 
 )
