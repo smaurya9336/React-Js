c3dab201ea0b548afd1530eddcf74fefda0a9552
Sarita Maurya <sm7767119@gmail.com>
2024-04-12 12:07:05 +0530

    react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Chai from "./chai"
 function App() {
+  const username="Sarita Maurya"
   return (
     <>
 
     <Chai />
-    <h1>hii Sarita</h1>
+    <h1>hii Sarita {username}</h1>
     <p>para</p>
    </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
+}
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+
+const reactElement = React.createElement(
+    'a',
+    {href :'https://google.com', target :'_blank'},
+    'click me to visite google'
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
+    // anotherElement
+    // reactElement
     <App />
   
 
