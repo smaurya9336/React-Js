7943cb60ea4140275563832bfef8a703177ba42e
Sarita Maurya <sm7767119@gmail.com>
2024-04-15 15:50:27 +0530

    bgChanger

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color,setColor]=useState("olive")
+
+  return (
+    <div className="w-full h-screen duration-200">
+     style={{backgroundColor:color}}
+    </div>
+  )
+}
+
+export default App
