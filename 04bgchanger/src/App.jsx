395a61bd5358e246be20abb1c6f431e06b823dbc
Sarita Maurya <sm7767119@gmail.com>
2024-04-16 13:55:06 +0530

    bgChanger project

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -5,8 +5,24 @@ function App() {
   const [color,setColor]=useState("olive")
 
   return (
-    <div className="w-full h-screen duration-200">
+    <div className="w-full h-screen duration-200"
      style={{backgroundColor:color}}
+     >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <button onClick={()=> setColor("red")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+        <button onClick={()=> setColor("blue")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"blue"}}>Blue</button>
+        <button onClick={()=> setColor("orange")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"orange"}}>Orange</button>    
+        <button onClick={()=> setColor("green")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>Green</button>
+        <button onClick={()=> setColor("magenta")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"magenta"}}>Magenta</button>
+        <button onClick={()=> setColor("purple")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"purple"}}>Purple</button>
+        <button onClick={()=> setColor("black")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"black"}}>Black</button>
+        <button onClick={()=> setColor("grey")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"grey"}}>Grey</button>
+        <button onClick={()=> setColor("skyblue")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor:"skyblue"}}>Skyblue</button>
+        
+        
+     </div>
+      </div>
     </div>
   )
 }
