34bc39f4d8fdf69008695fffe39a07c4416543b8
Sarita Maurya <sm7767119@gmail.com>
2024-04-19 13:48:17 +0530

    currency convertor

--- 06currencyConvertor/src/components/InputBox.jsx
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -35,7 +35,7 @@ return (
             <select
                 className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                 value={selectCurrency}
-                onChange={(e) => onCurrencyChange &&onCurrencyChange(e.target.value)}
+                onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                 disabled={currencyDisable}
                 
             >
