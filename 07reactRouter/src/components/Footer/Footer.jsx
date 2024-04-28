ec4c7310e32e25d1b2ecf06d077d015aa50e4f43
Sarita Maurya <sm7767119@gmail.com>
2024-04-28 13:40:04 +0530

    react router dom

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -72,9 +72,9 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        © 2024
+                        <a href="https://github.com/smaurya9336" className="hover:underline">
+                             saritamaurya
                         </a>
                         . All Rights Reserved.
                     </span>
